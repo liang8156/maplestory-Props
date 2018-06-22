57931cb597849d1a4f3acb22832b311f86b3c10c
alexLiang <liang8156@gmail.com>
2018-06-23 01:27:40 +0800

    基本變數設定

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
-
+let equipment = [], consumption = [], other = [], cash = []
+console.warn(equipment)
 document.addEventListener('keydown', function (e) {
+    console.warn(e.keyCode)
     if (e.keyCode === 73 || e.keyCode === 105) {
         if (document.getElementById("mybar").style.display == "inline") {
             document.getElementById("mybar").style.display = "none"
