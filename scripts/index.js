c357dc68eee518fd9a38bfd0c3171820f1f9e7f6
liang <liang8156@gmail.com>
2018-06-30 15:39:58 +0800

    裝備分類到bar

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,45 @@
-let bar = { equipment: [], consumption: [], other: [], cash: [] }
+let backpack = { equipment: [], consumption: [], other: [], cash: [] }
 let a = {
-    class: "equipment"
+    class: "equipment",
+    name: "e1"
 }
 let b = {
-    class: "consumption"
+    class: "consumption",
+    name: "c1"
 }
 let c = {
-    class: "equipment"
+    class: "equipment",
+    name: "e2"
 }
 let d = {
-    class: "other"
+    class: "other",
+    name: "o1"
 }
-let total = { a, b, c, d }
-function putIntoBar(ab) {
-    console.warn("分類中", ab)
+let f = {
+    class: "cash",
+    name: "ca1"
 }
-putIntoBar(total)
-console.warn(bar)
-console.warn(bar.equipment)
-console.warn(total)
+
+let e = {
+    class: "cash",
+    name: "ca2"
+}
+
+function putIntoBar(stuff, bar) {
+    let stuffKey = Object.keys(stuff)
+    let barKey = Object.keys(bar)
+    for (let i = 0; i < stuffKey.length; i++) {
+        for (let j = 0; j < barKey.length; j++) {
+            if (stuff[stuffKey[i]].class === barKey[j]) {
+                bar[barKey[j]] = bar[barKey[j]].concat([stuff[stuffKey[i]]])
+            }
+        }
+    }
+    // return bar
+}
+putIntoBar({ a, b, c, d, f }, backpack)
+putIntoBar({ e }, backpack)
+console.warn("backpack", backpack)
 document.addEventListener('keydown', function (e) {
     console.warn(e.keyCode)
     if (e.keyCode === 73 || e.keyCode === 105) {
